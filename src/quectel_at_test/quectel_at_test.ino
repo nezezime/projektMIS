d7ac7f1710719b8813962786c8ec658916119137
jk <jkopit1@gmail.com>
2018-04-10 19:25:27 +0200

    Create quectel_at_test.ino

---
+++ src/quectel_at_test/quectel_at_test.ino
@@ -0,0 +1,33 @@
+//includes
+#include "quectel_arduino_lib.h"
+
+//macros
+#define QUECTEL_SERIAL_RX 8
+#define QUECTEL_SERIAL_TX 7
+
+extern quectelArduinoClass quectelArduino;
+
+void setup() {
+
+  //init serial communication
+  Serial.begin(9600);
+  
+  if(quectelArduino.init(QUECTEL_SERIAL_RX, QUECTEL_SERIAL_TX, "8", "v-iot", "29431") == false){
+    Serial.println("Failed quectel module initialization");
+  }
+  else Serial.println("Quectel module serial communication successful");
+}
+
+void loop() {
+  quectelArduino.send_at_command("AT");
+  char * resp = quectelArduino.get_at_response();
+  //Serial.print("a");
+  //Serial.print(resp);
+  //Serial.print("b");
+  delay(1000);
+
+  char bla;
+
+  Serial.println(quectelArduino.crop_at_response());
+
+}
