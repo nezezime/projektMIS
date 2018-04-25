9a120dba3a379fcd00d5ae0cdb65c701d3307ab8
nihce <nihce@github.com>
2018-04-25 16:33:04 +0200

    preureditev vrstnega reda ukazov

--- src/accel/accel.ino
+++ src/accel/accel.ino
@@ -12,23 +12,27 @@ void setup() {
   adxl.powerOn();
   adxl.setRangeSetting(2);           // Give the range settings
                                       // Accepted values are 2g, 4g, 8g or 16g
-                                      // Higher Values = Wider Measurement Range
-                                      // Lower Values = Greater Sensitivity
   
   adxl.setActivityXYZ(1,1,1);       // Set to activate movement detection in the axes "adxl.setActivityXYZ(X, Y, Z);" (1 == ON, 0 == OFF)
   adxl.setActivityThreshold(30);      // 62.5mg per increment   // Set activity   // Inactivity thresholds (0-255)
 
   // Setting all interupts to take place on INT1 pin
-  adxl.setImportantInterruptMapping(1, 1, 1, 1, 1);     // Sets "adxl.setEveryInterruptMapping(single tap, double tap, free fall, activity, inactivity);" 
+  adxl.setImportantInterruptMapping(0, 0, 0, 1, 0);     // Sets "adxl.setEveryInterruptMapping(single tap, double tap, free fall, activity, inactivity);" 
                                                         // Accepts only 1 or 2 values for pins INT1 and INT2. This chooses the pin on the ADXL345 to use for Interrupts.
                                                         // This library may have a problem using INT2 pin. Default to INT1 pin.
+
+  // Turn on Interrupts for each mode (1 == ON, 0 == OFF)
+  adxl.InactivityINT(0);
+  adxl.ActivityINT(1);
+  adxl.FreeFallINT(0);
+  adxl.doubleTapINT(0);
+  adxl.singleTapINT(0);
+  /*
   adxl.setInterruptLevelBit(1); // positive logic
-  
   pinMode(interruptPin, INPUT);
+  */
   attachInterrupt(digitalPinToInterrupt(interruptPin), ADXL_ISR, RISING);
   
-  // Turn on Interrupt (1 == ON, 0 == OFF)
-  adxl.ActivityINT(1);
 
   Serial.println("END SETUP");
 }
